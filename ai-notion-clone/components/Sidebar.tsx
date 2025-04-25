eea7a57281d88673e51b51f950f4a2c74c1f71f4
Ankita Goyal <goyalankita0325@gmail.com>
2025-04-25 13:59:36 +0530

    Working on Sidebar

--- ai-notion-clone/components/Sidebar.tsx
+++ ai-notion-clone/components/Sidebar.tsx
@@ -1,12 +1,37 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 import NewDocumentButton from "./NewDocumentButton"
 
-function Sidebar  () {
-  return (
-    <div className="p-2 md:p-5">
-        <NewDocumentButton/>
-    </div>
-    
-  )
+
+function Sidebar() {
+    return (
+        <div className="p-2 md:p-5 bg-gray-200 relative">
+
+            <Sheet>
+                <SheetTrigger>Open</SheetTrigger>
+                <SheetContent side="left">
+                    <SheetHeader>
+                        <SheetTitle>Are you absolutely sure?</SheetTitle>
+                        <SheetDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+
+            <div className="hidden md:inline">
+                <NewDocumentButton />
+            </div>
+        </div>
+
+    )
 }
 
 export default Sidebar
