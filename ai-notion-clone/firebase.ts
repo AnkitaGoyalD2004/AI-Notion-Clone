2fa00b2a9d8118e4ee70b67feb4b3b99e449cdac
Ankita Goyal <goyalankita0325@gmail.com>
2025-04-26 17:59:39 +0530

    working on firebase

--- ai-notion-clone/firebase.ts
+++ ai-notion-clone/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
 import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app =  getApps.length === 0 ? initializeApp(firebaseConfig): getApp();
-const analytics = getAnalytics(app);
+const app =  getApps().length === 0 ? initializeApp(firebaseConfig): getApp();
+const db = getFirestore(app);
+
+export { db };
